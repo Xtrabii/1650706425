621ffb58b1ac4c4d56c60edcd86d784996ddf3f1
Nabi <164737439+Xtrabii@users.noreply.github.com>
2025-02-21 02:08:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, BU1650706425!'
+    return 'Hello, BU :D'
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
